8a5b6fee3bee9c1feb714433fd5c08929789a998
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-01-13 23:29:35 +0530

    Add files via upload

---
+++ LL13.C
@@ -0,0 +1,98 @@
+// Program to Reverse the Linked List.
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+	int data;
+	struct node *next;
+}node;
+
+node *createList(int);
+void displayList(node *);
+node *reverseList(node *);
+
+void main()
+{
+	node *head=NULL, *p;
+	int n;
+
+	clrscr();
+
+	printf("Enter Number of Nodes: ");
+	scanf("%d",&n);
+
+	head=createList(n);
+	displayList(head);
+	p=reverseList(head);
+	displayList(p);
+
+	getch();
+}
+node *createList(int n)
+{
+	node *p, *temp, *head=NULL;
+	int i;
+
+	printf("\n");
+
+	for(i=0;i<n;i++)
+	{
+		temp=(node *)malloc(sizeof(node));
+		printf("Enter Data for Node[%d]: ", i+1);
+		scanf("%d",&temp->data);
+
+		temp->next=NULL;
+
+		if(head==NULL)
+			head=temp;
+		else
+		{
+			p=head;
+
+			while(p->next!=NULL)
+			{
+				p=p->next;
+			}
+			p->next=temp;
+		}
+	}
+	return head;
+}
+void displayList(node *head)
+{
+	node *p;
+
+	printf("\n");
+
+	if(head==NULL)
+		printf("\nList is Empty!");
+	else
+	{
+		p=head;
+
+		while(p!=NULL)
+		{
+			printf("[%d]=>",p->data);
+			p=p->next;
+		}
+	}
+}
+node *reverseList(node *head)
+{
+	node *p, *t1=NULL, *t2=NULL;
+	p=head;
+
+	while(p!=NULL)
+	{
+		t2=p->next;
+		p->next=t1;
+		t1=p;
+		p=t2;
+	}
+	printf("\n\nReversed List:\n");
+
+	return (t1);
+}
