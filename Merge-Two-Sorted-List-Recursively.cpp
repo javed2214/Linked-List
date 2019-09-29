a76e373524f9f54ea3a93fc9a5378488adf257b5
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-09-29 22:53:45 +0530

    Update and rename MergeTwoSortedListUsingRecursion.cpp to Merge-Two-Sorted-List-Recursively.cpp

---
+++ Merge-Two-Sorted-List-Recursively.cpp
@@ -0,0 +1,20 @@
+// Program to Merge Two Linked List Recursively
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        if(l1->val <= l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else{
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
