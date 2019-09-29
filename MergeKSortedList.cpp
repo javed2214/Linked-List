cfa029f6ca5abd7ddf839cd2c44baf0b02f1e195
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-09-29 22:40:06 +0530

    Update MergeKSortedList.cpp

--- MergeKSortedList.cpp
+++ MergeKSortedList.cpp
@@ -1,28 +1,31 @@
 // https://leetcode.com/problems/merge-k-sorted-lists/
 
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
                 
-        priority_queue<int, vector<int>, greater<int>> pq;
+        priority_queue<pair<int, ListNode *>, vector<pair<int, ListNode *>>, greater<pair<int, ListNode *>>> pq;
         
         for(int i=0;i<lists.size();i++){
             ListNode *head = lists[i];
             while(head != NULL){
-                pq.push(head->val);
+                pq.push({head->val, head});
                 head = head->next;
             }
         }
+      
         ListNode *head = NULL, *p = NULL;
+        
         while(!pq.empty()){
-            ListNode *temp = new ListNode(pq.top());
+            ListNode *temp = pq.top().second;
             if(head == NULL) head = temp, p = temp;
             else{
                 p->next = temp;
                 p = p->next;
             }
             pq.pop();
-        }       
+        }
         return head;
     }
 };
